aa88990f660ad052d99c6846580a180dadad1039
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-10-03 06:52:28 +0530

    Add first animation

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let totalStars = [];
+let speed = 5;
+let canvas;
+let ctx;
+let center;
 
 function canvasHW() {
-    let canvas = document.getElementById("myCanvas");
+    canvas = document.getElementById("myCanvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    let ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
     
 
     for (let i = 0; i < 100; i++) {
@@ -14,8 +18,10 @@ function canvasHW() {
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height
         }
+        totalStars.push(loc);
         stars(ctx, loc);
     }
+    animate();
 }
 
 function stars(ctx, loc) {
@@ -24,5 +30,29 @@ function stars(ctx, loc) {
         ctx.arc(loc.x, loc.y, loc.r, 0, 2 * Math.PI);
         ctx.fillStyle = "white";
         ctx.fill();
+}
+
+function updateStars(loc) {
+    center = {
+        x: window.innerWidth/2,
+        y: window.innerHeight/2
+    }
+    let angle = Math.atan2(
+        loc.y - center.y,
+        loc.x - center.x
+    );
+    loc.x += speed*Math.cos(angle);
+    loc.y += speed*Math.sin(angle);
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i=0; i<100; i++) {
+        console.log(i);
+        updateStars(totalStars[i]);
+        stars(ctx, totalStars[i]);
+    }
+
+    window.requestAnimationFrame(animate);
 }
 
