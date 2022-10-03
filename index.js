240abbb0d05afa73e63dfeb3da4e8ffa814aa6bc
Ratnesh Mishra <ratneshmishrarulz@gmail.com>
2022-10-03 13:08:10 +0530

    Add stretchinng stars animation

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let totalStars = [];
+// let fixedStars = [];
 let speed = 5;
 let canvas;
 let ctx;
-let center;
+let center = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+}
+let pastLoc;
 
 function canvasHW() {
     canvas = document.getElementById("myCanvas");
@@ -10,44 +15,98 @@ function canvasHW() {
     canvas.height = window.innerHeight;
 
     ctx = canvas.getContext("2d");
+
     
+
 
     for (let i = 0; i < 100; i++) {
         let loc = {
-            r: (Math.random()*2) + 1,
+            r: (Math.random() * 2) + 1,
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height
+        }
+
+        pastLoc = {
+            pX: loc.x,
+            pY: loc.y
         }
         totalStars.push(loc);
         stars(ctx, loc);
     }
+
+    // for (let i = 0; i < 25; i++) {
+    //     let fixedLoc = {
+    //         r: (Math.random() * 2) + 1,
+    //         x: Math.random() * canvas.width,
+    //         y: Math.random() * canvas.height
+    //     }
+    //     fixedStars.push(fixedLoc);
+    //     stars(ctx, fixedLoc);
+    // }
     animate();
 }
 
-function stars(ctx, loc) {
-        console.log("hello");
-        ctx.beginPath();
-        ctx.arc(loc.x, loc.y, loc.r, 0, 2 * Math.PI);
-        ctx.fillStyle = "white";
-        ctx.fill();
+function stars(ctx, location) {
+    console.log("hello");
+    ctx.beginPath();
+
+    ctx.moveTo(location.x, location.y);
+    ctx.lineTo(pastLoc.pX, pastLoc.pY);
+    // console.log(loc.x, pastLoc.pX);
+
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = location.r;
+    ctx.stroke();
+
+    // ctx.arc(loc.x, loc.y, loc.r, 0, 2 * Math.PI);
+    // ctx.fillStyle = "white";
+    // ctx.fill();
 }
 
-function updateStars(loc) {
-    center = {
-        x: window.innerWidth/2,
-        y: window.innerHeight/2
-    }
+function updateStars(location) {
+    
     let angle = Math.atan2(
-        loc.y - center.y,
-        loc.x - center.x
+        location.y - center.y,
+        location.x - center.x
     );
-    loc.x += speed*Math.cos(angle);
-    loc.y += speed*Math.sin(angle);
+
+    pastLoc = {
+        pX: location.x,
+        pY: location.y
+    }
+
+
+    location.x += speed * Math.cos(angle);
+    location.y += speed * Math.sin(angle);
+
+    // console.log(pastLoc.pX, location.x);
+
+    if (location.x > window.innerWidth ||
+        location.x < 0 || location.y < 0 ||
+        location.y > window.innerHeight) {
+        location.x = Math.random() * window.innerWidth;
+        location.y = Math.random() * window.innerHeight;
+
+        pastLoc = {
+            pX: location.x,
+            pY: location.Y
+        }
+    }
+
+    let distanceFromCenter = Math.sqrt(
+        Math.pow(location.x - center.x, 2) +
+        Math.pow(location.y - center.y, 2)
+    )
+
+    location.r = 1+3*distanceFromCenter/window.innerWidth;
 }
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for(let i=0; i<100; i++) {
+    // for (let i = 0; i < 25; i++) {
+    //     stars(ctx, fixedStars[i]);
+    // }
+    for (let i = 0; i < 100; i++) {
         console.log(i);
         updateStars(totalStars[i]);
         stars(ctx, totalStars[i]);
